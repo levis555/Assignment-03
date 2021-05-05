8d37183c57e5f578f74eeb84d8c08767cbd2e5cb
azakarolina <sassastar@gmail.com>
2021-05-05 03:29:45 -0700

    azas commit

--- cypress/integration/aza-case.spec.js
+++ cypress/integration/aza-case.spec.js
@@ -1,50 +1,39 @@
-cy.log('At login page!') 
-    cy.per/// <reference types="cypress" />
+/// <reference types="cypress" />
 
-describe('First example', () => {
-    it('Test case 1', () => {
-    cy.visit('http://localhost:3000/login')
-    cy.title().should('eq','Testers Hotel')
-    cy.contains('Login')
-  cySnapshot('index-page')
+describe("Create Bill", () => {
+  it("login", () => {
+      cy.visit("http://localhost:3000");
+      cy.get('h2').should("contain", "Login")
 
-    cy.get(':nth-child(1) > input').type('tester01')
-    cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
-    cy.get('.btn').click()
-    cy.contains('Welcome')
-    cy.wait(3000)
-    
-    cy.log('At dashboard page!')
-    cy.percySnapshot('dashboard-page')
+      cy.get(":nth-child(1) > input").type("tester01")
+      cy.get(":nth-child(2) > input").type("GteteqbQQgSr88SwNExUQv2ydb7xuf8c")
 
-    cy.get('.blocks > :nth-child(2) > .btn').click()
-    cy.log('At client page!')
-    cy.contains('Clients')
-    cy.percySnapshot('clients page')
+      cy.get(".btn").click();
+      cy.wait(500)
+      cy.get(".username").should("contain", "Welcome tester01!")
 
-    cy.get(':nth-child(3) > .btn').click()
-    cy.log('At bills page!')
-    cy.contains('Bills')
-    cy.percySnapshot('bills page')
+      cy.get("h2").should("contain", "Tester Hotel Overview")
 
-    cy.get(':nth-child(4) > .btn').click()
-    cy.log('At reservation page!')
-    cy.contains('Reservations')
-    cy.percySnapshot('reservation page')
+      cy.get(":nth-child(3) > .btn").click();
 
-    cy.get(':nth-child(1) > .btn').click()
-    cy.log('At room page!')
-    cy.contains('Room')
-    cy.percySnapshot('room page')
+      cy.get("h2 > div").should("contain", "Bills")
 
-    
-    
-    
+      cy.get("h2 > .btn").click();
 
+      cy.get("h2 > div").should("contain", "New Bill")
 
+      cy.get("input").type(9900)
 
-    
-    
-    })
+      cy.get(".blue").click();
 
-})
+      cy.get(":nth-child(2) > .value").should("contain", "9900kr")
+
+      cy.get(":nth-child(2) > .action > img").click();
+
+      cy.get(".menu > :nth-child(2)").click(); 
+
+      cy.get(".user > .btn").click();
+
+      cy.get("h2").should("contain", "Login")
+  });
+});
